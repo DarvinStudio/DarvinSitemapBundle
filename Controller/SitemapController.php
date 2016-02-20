daedf3c95dd9d7d3d0253d39bf01fe49ebbe344e
igor <igor.sv.n@gmail.com>
2016-02-20 14:46:24 +0300

    Implement sitemap controller.

---
+++ Controller/SitemapController.php
@@ -0,0 +1,38 @@
+<?php
+/**
+ * @author    Igor Nikolaev <igor.sv.n@gmail.com>
+ * @copyright Copyright (c) 2016, Darvin Studio
+ * @link      https://www.darvin-studio.ru
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Darvin\SitemapBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Response;
+
+/**
+ * Sitemap controller
+ */
+class SitemapController extends Controller
+{
+    /**
+     * @return \Symfony\Component\HttpFoundation\Response
+     */
+    public function sitemapAction()
+    {
+        return new Response($this->getSitemapGenerator()->generateSitemap(), 200, array(
+            'Content-Type' => 'text/xml',
+        ));
+    }
+
+    /**
+     * @return \Darvin\SitemapBundle\Generator\SitemapGeneratorInterface
+     */
+    private function getSitemapGenerator()
+    {
+        return $this->get('darvin_sitemap.generator');
+    }
+}
